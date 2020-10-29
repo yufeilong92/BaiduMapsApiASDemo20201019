01b1700cf5815b4608a64400938b141b7f277673
Dell <931697478@qq.com>
2020-10-29 10:38:34 +0800

    Initial commit1

---
+++ app/src/main/java/baidumapsdk/demo/geometry/LatLngAnimation.java
@@ -0,0 +1,19 @@
+package baidumapsdk.demo.geometry;
+
+import com.baidu.mapapi.model.LatLng;
+
+/**
+ * Copyright (C) 2019 Baidu, Inc. All Rights Reserved.
+ */
+public class LatLngAnimation {
+
+    private LatLng position;
+
+    public LatLng getPosition() {
+        return position;
+    }
+
+    public void setPosition(LatLng position) {
+        this.position = position;
+    }
+}
